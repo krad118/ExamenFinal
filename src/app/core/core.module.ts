43ef407f5c0cd82a95257cf52a02274f79e20f8e
krad118 <krad118@gmail.com>
2021-01-09 18:35:05 -0500

    feat(1e): Creación página control de errores 404

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './componets/navbar/navbar.component';
+import { NotFoundComponent } from './componets/not-found/not-found.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, NotFoundComponent],
   imports: [
     CommonModule,
     RouterModule
