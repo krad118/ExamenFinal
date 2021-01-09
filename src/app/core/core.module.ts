21256ba7f232eb320545f23778676075bffe987d
krad118 <krad118@gmail.com>
2021-01-09 17:26:52 -0500

    feat(1c): Agregado estilos bootstrap 4 para el maquetado de la plataforma

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './componets/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule
   ]
